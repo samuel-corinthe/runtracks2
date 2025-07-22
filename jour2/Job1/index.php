48348958635dbe0685c398708fc59ea5e3ea1cfc
samuel-corinthe <samuel.corinthe@hotmail.fr>
2025-07-22 09:27:31 +0200

    commit

---
+++ jour2/Job1/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$num=0;
+
+
+
+for ($num=0; $num<=1337; $num++)
+ { 
+    for ($num=0; $num=42; "<b><u>42</b></u>"){
+        echo  $num . "<br>"  ;
+    }
+    echo  $num . "<br>"  ;
+}
+
+
