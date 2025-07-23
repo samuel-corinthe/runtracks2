49d097445f5e5b2f7b0b2733fff1f556788d5f1b
samuel-corinthe <samuel.corinthe@hotmail.fr>
2025-07-23 13:18:02 +0200

    commit-jour-3

--- jour 3/Job 07/Index.php
+++ jour 3/Job 07/Index.php
@@ -1,8 +1,14 @@
 <?php 
-$str = "Les choses que l'on possède finissent par nous posséder";
-$strArray = mb_str_split($str);
-array_splice($strArray, 1, 2);
-foreach ($strArray as $char) {
-    
-    echo $char;
-}
+$str = "Certaines choses changent, et d'autres ne changeront jamais.";
+
+$str = substr($str, 1) . $str[0];
+
+
+
+echo $str;
+
+
+
+
+ 
+
